d4d868ac6d37e183b23e0ecf2f74c8364ff96327
YAO SONG <songyaos@sfu.ca>
2015-02-13 04:20:56 -0800

    successfully run on sample dataset

--- src/PageRank/PageRank.java
+++ src/PageRank/PageRank.java
@@ -10,8 +10,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -19,16 +17,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileAsTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+//import org.apache.log4j.Logger;
 
 public class PageRank {
-	private static int PAGES_NUM = 5716808;
+	private static int PAGES_NUM = 6;//5716808;
 	
 	public static class Map extends Mapper<Text, Text,Text,Text >{
+		//private static final Logger sLogger = Loggr.getLogger(Map.class);
 		private String page_info = "";
 		private String[] page_info_list = null;
 		private String[] neighbors = null;
@@ -38,11 +38,12 @@ public class PageRank {
 		public void map(Text page_id_text, Text page_info_text, Context context) 
 				throws IOException, InterruptedException{
 			context.write(page_id_text,page_info_text);
-			page_info = page_info_text.toString();
-			page_info_list = page_info.split(",");
-
+			page_info = page_info_text.toString().trim();
+			page_info_list = page_info.split("-");
 			this_page_current_rank = Double.parseDouble(page_info_list[0]);
 			neighbors = page_info_list[1].split("\\s+");//split by space
+			
+			
 			out_page_rank = this_page_current_rank/neighbors.length;
 			
 			for (String neighbor: neighbors){
@@ -62,14 +63,14 @@ public class PageRank {
 				throws IOException, InterruptedException{
 			sum = 0;
 			for(Text page_info_text : page_info_list){
-				String[] page_info = page_info_text.toString().split(",");
-				if (page_info.length == 2){
-					neighbors = page_info[1];
+				String[] page_info = page_info_text.toString().split("-");
+				if (page_info.length > 1){
+					neighbors = page_info[1].trim();
 				}
 				sum += Double.parseDouble(page_info[0]);
 			}
 			out_page_rank = DAMPING*sum + (1-DAMPING)/PAGES_NUM;
-			output_page_info = Double.toString(out_page_rank) + "," + neighbors;
+			output_page_info = Double.toString(out_page_rank) + "-" + neighbors;
 			
 			context.write(page_id,new Text(output_page_info));
 		}
@@ -88,7 +89,6 @@ public class PageRank {
 		job.setJarByClass(PageRank.class);
 		job.setMapperClass(Map.class);
 		job.setReducerClass(Reduce.class);
-		
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(Text.class);
 		job.setOutputKeyClass(Text.class);
@@ -125,9 +125,9 @@ public class PageRank {
 		String neighbors;
 		while((line=input_buffer.readLine()) != null){
 			items = line.split(":");
-			page_id = items[0];
-			neighbors = items[1];
-			writer.append(new Text(page_id), new Text(INITIAL_RANK + "," + neighbors));
+			page_id = items[0].trim();
+			neighbors = items[1].trim();
+			writer.append(new Text(page_id), new Text(INITIAL_RANK + "-" + neighbors));
 		}
 		writer.close();
 		fileReader.close();
@@ -136,6 +136,16 @@ public class PageRank {
 		//update page rank loop 5 iterations.
 		for(int i= 1;i<=MAX_ITER;i++){
 			job = Job.getInstance(conf);
+			job.setJarByClass(PageRank.class);
+			job.setMapperClass(Map.class);
+			job.setReducerClass(Reduce.class);
+			job.setMapOutputKeyClass(Text.class);
+			job.setMapOutputValueClass(Text.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(Text.class);
+			job.setInputFormatClass(SequenceFileInputFormat.class);
+			job.setOutputFormatClass(SequenceFileOutputFormat.class);
+			
 			FileInputFormat.setInputPaths(job, new Path(INNER_FILE_PATH + Integer.toString(i-1)));//read from and write to FORMAT_PATH
 			if (i==MAX_ITER)//final output path
 			{
